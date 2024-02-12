5836e4b69011466792de4f6b10b0aaa6215614b0
karisha06 <roverova1@icloud.com>
2024-02-12 16:16:48 +0300

    Решили задачу с помощью циклов

--- task4/main.js
+++ task4/main.js
@@ -1,48 +1,48 @@
-// const array = [ [34, 43], [56, 70] ]; 
+const array = [ [46, 22], [17, 94] ]; 
  
-// let summ = 0; 
+let summ = 0; 
  
-// for (let i = 0; i < array.length; i++) { 
-//     for (let j = 0; j < array[i].length; j++){ 
-//         console.log(array[i][j]); 
-//     } 
-// } 
+for (let i = 0; i < array.length; i++) { 
+    for (let j = 0; j < array[i].length; j++){ 
+        console.log(array[i][j]); 
+    } 
+ } 
  
-// for (let i = 0; i < array.length; i++) { 
-//     console.log(`кол-во элементов в ${i+1} массиве = ${array[i].length}`); 
-//     for (let j = 0; j < array[i].length; j++) { 
-//         summ += array[i][j]; 
-//     } 
-// } 
-// console.log(`Сумма элементов массива = ${summ}.`); 
+ for (let i = 0; i < array.length; i++) { 
+     console.log(`кол-во элементов в ${i+1} массиве = ${array[i].length}`); 
+     for (let j = 0; j < array[i].length; j++) { 
+         summ += array[i][j]; 
+    } 
+ } 
+ console.log(`Сумма элементов массива = ${summ}.`); 
  
  
-// const users = [ 
-//     { name: 'Alice', age: 25, city: 'New York' }, 
-//     { name: 'Anna', age: 14, city: 'Moscow' }, 
-//     { name: 'Lisa', age: 35, city: 'Minsk' } 
-// ]; 
+ const users = [ 
+     { name: 'Vlada', age: 18, city: 'London' }, 
+     { name: 'Alekc', age: 10, city: 'Volgograd' }, 
+    { name: 'Ksenia', age: 40, city: 'Vladikavkaz' } 
+ ]; 
  
-// for (let user of users) { 
-//     let age = user.age 
-//     console.log(age); 
-// } 
+ for (let item of users) { 
+     let age = item.age 
+     console.log(age); 
+ } 
  
-// for (let user of users) { 
-//     user.age += 1 
-//     console.log(user); 
-// } 
+ for (let item of users) { 
+     item.age += 1 
+     console.log(age); 
+ } 
  
  
 const companies = { 
     companyA: { 
-        employees: ['Mariya', 'Ivan'] 
+        employees: ['Viktor', 'Nastya'] 
     }, 
     companyB: { 
-        employees: ['Bob', 'Dan'] 
+        employees: ['Ellen', 'Set'] 
     }, 
     companyC: { 
-        employees: ['Eve', 'Steve'] 
+        employees: ['Nil', 'Andry'] 
     } 
 }; 
  
@@ -50,6 +50,5 @@ for (let company in companies) {
     console.log(`Сотрудники компании ${company}:`); 
     for (let i = 0; i < companies[company].employees.length; i++) { 
         console.log(companies[company].employees[i]); 
-    } 
-    console.log("============") 
+    }  
 }
